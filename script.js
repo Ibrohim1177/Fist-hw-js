a6228729c4b966cf26f7a55f8a47b9aa7383a0a0
Ibrohim1177 <bakhadirov1177@gmail.com>
2020-10-02 16:56:56 +0500

    Fist homework js

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+var name = prompt("Ismingizni kiriting");
+console.log("Mening ismim " + name);
+var age = prompt("Yoshingizni kirirng");
+console.log("Mening yoshim " + age + " da");
+
+var misol1 = prompt("Misolni yeching 9 + 6 = ?");
+console.log("Misol 1: 9 + 6 = 15 Sizning javob " + misol1);
+var misol2 = prompt("Misolni yeching 7 - 4 = ?");
+console.log("Misol 1: 7 - 4 = 15 Sizning javob " + misol2);
+var misol3 = prompt("Misolni yeching 7 * 7 = ?");
+console.log("Misol 1: 7 * 7 = 49 Sizning javob " + misol3);
+var misol4 = prompt("Misolni yeching 9 / 3 = ?");
+console.log("Misol 1: 9 / 3 = 3 Sizning javob " + misol4);
+var misol5 = prompt("Misolni yeching 7 / 2 = ?");
+console.log("Misol 1: 7 / 2 = 1 Sizning javob " + misol5);
+alert("Natijani korish uchun console ni oching!")
